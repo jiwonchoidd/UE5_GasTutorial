ef65402f542b5baeff89170deb5aeda6e530c018
Jiwon <jae2021@gmail.com>
2025-07-06 23:02:58 +0900

    기본 튜토리얼 맵 커밋
    기본 튜토리얼 맵 커밋

---
+++ Source/GasTutorial/GasTutorial.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GasTutorial : ModuleRules
+{
+	public GasTutorial(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"GasTutorial",
+			"GasTutorial/Variant_Platforming",
+			"GasTutorial/Variant_Combat",
+			"GasTutorial/Variant_Combat/AI",
+			"GasTutorial/Variant_SideScrolling",
+			"GasTutorial/Variant_SideScrolling/Gameplay",
+			"GasTutorial/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/GasTutorial/GasTutorialGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GasTutorialGameMode.h"
+
+AGasTutorialGameMode::AGasTutorialGameMode()
+{
+	// stub
+}
